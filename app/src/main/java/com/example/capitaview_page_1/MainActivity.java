9e3d6963f7100bc613caf52a078620b85d07e729
Rahul Nair <rahulnair14032002@gmail.com>
2024-02-21 23:23:11 +0530

    Registration Page Added

--- app/src/main/java/com/example/capitaview_page_1/MainActivity.java
+++ app/src/main/java/com/example/capitaview_page_1/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.capitaview_page_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView signInLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInLink = (TextView) findViewById(R.id.loginSignInLink);
+        signInLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this,Registration_activity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/capitaview_page_1/Registration_activity.java
@@ -0,0 +1,14 @@
+package com.example.capitaview_page_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
