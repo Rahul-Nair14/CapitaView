33a2e375dcb6535bba4a78c80ed7d3d985f1039f
Rahul Nair <rahulnair14032002@gmail.com>
2024-02-22 21:38:15 +0530

    Addition of Validation - I

--- app/src/main/java/com/example/capitaview_page_1/MainActivity.java
+++ app/src/main/java/com/example/capitaview_page_1/MainActivity.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView signInLink;
+    EditText loginUserNameVar, loginPasswordVar;
+    Button loginButtonVar;
+
 
 
     @Override
@@ -19,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         signInLink = (TextView) findViewById(R.id.loginSignInLink);
+        loginUserNameVar = (EditText)findViewById(R.id.LoginPageUserNameEntry);
+        loginPasswordVar = (EditText)findViewById(R.id.LoginPagePassWordEntry);
+        loginButtonVar = (Button)findViewById(R.id.LoginButton);
+
         signInLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,5 +46,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        loginButtonVar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String userName = loginUserNameVar.getText().toString();
+                String passWord = loginPasswordVar.getText().toString();
+
+                if(isValidEmail(userName)) {
+
+                    Toast.makeText(getApplicationContext(),"Welcome "+ userName,Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,MainDashboard.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Invalid Username or password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    //Checks if email is of valid format
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
 }
